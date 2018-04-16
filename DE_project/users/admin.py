b40081e9cb1403949128040b187700cc96e2e2d6
Esen9248 <eee.coool@gmail.com>
2018-04-16 20:34:13 +0600

    16.04.18 users commit

--- DE_project/users/admin.py
+++ DE_project/users/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from .models import Users, Users_number
+from .models import Users
 # Register your models here.
 admin.site.register(Users)
-admin.site.register(Users_number)
