1fd29df610c2c4a20c80b4e12922132d170041f4
Esen9248 <eee.coool@gmail.com>
2018-04-20 21:41:59 +0600

    20.04 login

--- DE_project/users/forms.py
+++ DE_project/users/forms.py
@@ -1,8 +1,13 @@
 from django import forms
-from models import *
+
 
 class AddUserForm(forms.Form):
     name = forms.CharField(max_length=255)
     sur_name = forms.CharField(max_length=255)
     middle_name = forms.CharField(max_length=255)
     number = forms.IntegerField()
+    info = forms.CharField(widget=forms.Textarea)
+
+class LoginUser(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
--- DE_project/users/views.py
+++ DE_project/users/views.py
@@ -1,10 +1,33 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Users
+from django.contrib.auth import authenticate, login, logout
+from .forms import *
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+@login_required
 def users_view(request):
     users = Users.objects.all()
     return render(request, 'users/users.html', locals())
-
+@login_required
 def user_details(request, slug):
     user = get_object_or_404(Users, slug=slug)
     return render(request, 'users/details.html', locals())
+
+def login_user(request):
+    next_url = request.GET.get('next', reverse('users'))
+    login_user_form = LoginUser(request.POST or None)
+    if login_user_form.is_valid():
+        username = login_user_form.cleaned_data['username']
+        password = login_user_form.cleaned_data['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect(next_url)
+        else:
+            error_message = 'Can`t login' 
+    return render(request, 'loginuser.html', locals())
+@login_required
+def logout_user(request):
+    logout(request)
+    return redirect(reverse('login-user'))
