97dc59a7f79bb76d55ce97d0fa9987c45bc34d5b
Esen9248 <eee.coool@gmail.com>
2018-04-25 21:54:31 +0600

    register header

--- DE_project/orders/urls.py
+++ DE_project/orders/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.orders, name='order'),
+    path('', views.orders, name='orders'),
     path('post-order/', views.post_order, name='post-order'),
     path('<str:slug>/', views.order_details, name='order-details')
 ]
--- DE_project/users/forms.py
+++ DE_project/users/forms.py
@@ -1,20 +1,35 @@
 from django import forms
 from .models import Users
+from django.contrib.auth.models import User as DjangoUser
+from django.utils.text import slugify
 
 class AddUserForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
     name = forms.CharField(max_length=255)
     sur_name = forms.CharField(max_length=255)
     middle_name = forms.CharField(max_length=255)
     number = forms.IntegerField()
-    info = forms.CharField(widget=forms.Textarea)
+    info = forms.CharField(widget=forms.Textarea, required=False)
     def save(self):
+        django_user = DjangoUser.objects.create(
+            username=self.cleaned_data['username'],
+            first_name=self.cleaned_data['name'],
+            last_name=self.cleaned_data['sur_name']
+        )
+        django_user.set_password(self.cleaned_data['password'])
+        django_user.save()
+        slug = slugify(self.cleaned_data['name'])
         Users.objects.create(
+            auth_user = django_user,
             name=self.cleaned_data['name'],
             sur_name=self.cleaned_data['sur_name'],
             middle_name=self.cleaned_data['middle_name'],
             number=self.cleaned_data['number'],
             info=self.cleaned_data['info'],
+            slug=slug
         )
+    
 
 class LoginUser(forms.Form):
     username = forms.CharField()
